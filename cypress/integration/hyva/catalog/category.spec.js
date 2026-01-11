b4aa8250e8cd1cb15a43a79d4ccded47f210a6e4
Vinai Kopp <vinai@netzarbeiter.com>
2026-01-11 12:29:23 +0100

    Improve category tests

--- cypress/integration/hyva/catalog/category.spec.js
+++ cypress/integration/hyva/catalog/category.spec.js
@@ -6,80 +6,69 @@ describe("Category page tests", () => {
         cy.visit(product.categoryUrl);
     });
 
-    it("Can visit the category page and filters on color red", () => {
-        cy.get(selectors.shopByColorFilter).contains('Color').click();
-        cy.get(selectors.selectColorRed).click();
+    it("Can filter products by color", () => {
+        cy.get(selectors.shopByColorFilter).contains("Color").click()
+        cy.get(selectors.selectColorRed).click()
+
         cy.get(selectors.activeFilterLabel).should("contain.text", "Color")
         cy.get(selectors.activeFilterValue).should("contain.text", "Red")
     });
 
-    it("Can sort products on price from lowest to highest", () => {
-        cy.get(selectors.sortBySelect).first().select(product.selectByPrice);
-        cy.get(selectors.productPriceDataAtt)
-            .eq(0)
-            .invoke("data", "price-amount")
-            .then((firstPrice) => {
-                cy.get(selectors.productPriceDataAtt)
-                    .eq(1)
-                    .invoke("data", "price-amount")
-                    .then((secondPrice) => {
-                        expect(firstPrice).to.be.lessThan(secondPrice);
-                    });
-            });
+    it("Can sort products by price (low to high)", () => {
+        cy.get(selectors.sortBySelect).first().select(product.selectByPrice)
+
+        // Store prices using aliases for cleaner comparison
+        cy.get(selectors.productPriceDataAtt).eq(0).invoke("data", "price-amount").as("firstPrice")
+        cy.get(selectors.productPriceDataAtt).eq(1).invoke("data", "price-amount").as("secondPrice")
+
+        cy.get("@firstPrice").then((firstPrice) => {
+            cy.get("@secondPrice").should("be.greaterThan", firstPrice)
+        })
     });
 
-    it("Can change the number of products to be displayed", () => {
+    it("Can change the number of products displayed", () => {
         cy.get(selectors.highestNumberOfProductsShowOption)
             .invoke("val")
-            .then((numberOfProducts) => {
-                cy.get(selectors.numberOfProductsSelect).first().select(
-                    numberOfProducts
-                );
+            .then((maxProducts) => {
+                cy.get(selectors.numberOfProductsSelect).first().select(maxProducts)
+
                 cy.get(selectors.numberOfShownItems)
                     .first()
-                    .should("have.text", numberOfProducts);
+                    .should("have.text", maxProducts)
+
                 cy.get(selectors.categoryProductContainer)
-                    .children()
-                    .should("to.have.length.of.at.most", +numberOfProducts);
-            });
+                    .should("have.length.at.most", parseInt(maxProducts, 10))
+            })
     });
 
     it("Can see the correct breadcrumbs", () => {
-        cy.get(selectors.breadcrumbsItem)
-            .first()
-            .should("contain.text", "Home");
-        cy.get(selectors.breadcrumbsItem)
-            .eq(1)
-            .should("contain.text", `${product.category}`);
-        cy.get(selectors.breadcrumbsItem)
-            .eq(2)
-            .should("contain.text", `${product.subCategory}`);
+        cy.get(selectors.breadcrumbsItem).eq(0).should("contain.text", "Home")
+        cy.get(selectors.breadcrumbsItem).eq(1).should("contain.text", product.category)
+        cy.get(selectors.breadcrumbsItem).eq(2).should("contain.text", product.subCategory)
     });
 
     it("Can switch between grid and list view", () => {
-        cy.get(selectors.categoryProductGridWrapper).should("be.visible");
-        cy.get(selectors.listModeButton).first().click();
-        cy.get(selectors.categoryProductListWrapper).should("be.visible");
+        cy.get(selectors.categoryProductGridWrapper).should("be.visible")
+
+        cy.get(selectors.listModeButton).first().click()
+
+        cy.get(selectors.categoryProductListWrapper).should("be.visible")
     });
 
-    it("Can move to the next page using the pages navigation", () => {
-        cy.get(".column > section").then(($mainColumn) => {
-            if ($mainColumn[0].querySelector(selectors.pageNavigation)) {
-                cy.get(selectors.pageNavigation)
-                    .first()
-                    .should("to.have.length.of.at.most", 6);
-                cy.get(selectors.pageLink).first().contains("2").click();
-                // Check that we are on the second page, either of these assertions would be fine?
-                cy.url().then((url) => {
-                    expect(url.includes("p=2")).to.be.true;
-                });
-                cy.get(selectors.secondPageItem)
-                    .first()
-                    .should("include.text", "2")
-                    .should("not.have.attr", "href");
-            } else {
-                cy.get(selectors.pageNavigation).should("not.exist");
+    it("Can navigate to the next page using pagination", function () {
+        // Skip test if pagination doesn't exist (not enough products)
+        cy.get("body").then(($body) => {
+            if (!$body.find(selectors.pageNavigation).length) {
+                this.skip()
             }
-        });
+        })
+
+        cy.get(selectors.pageNavigation).should("have.length.at.least", 6)
+        cy.get(selectors.pageLink).contains("2").click()
+
+        cy.url().should("include", "p=2")
+        cy.get(selectors.currentPageItem)
+            .should("contain.text", "2")
+            .and("have.attr", "aria-current", "page")
     });
 });
