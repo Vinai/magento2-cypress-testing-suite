a5ffd35be3229fa0283ec6a10ceee5b617ce05be
Vinai Kopp <vinai@netzarbeiter.com>
2026-01-11 11:56:36 +0100

    Remove random string causing error

--- cypress/page-objects/hyva/account.js
+++ cypress/page-objects/hyva/account.js
@@ -56,7 +56,6 @@ export class Account {
         cy.get(selectors.newPasswordConfirmationInputSelector).type(
             `${passwd}{enter}`
         );
-        slee
     }
 
     static logout() {
